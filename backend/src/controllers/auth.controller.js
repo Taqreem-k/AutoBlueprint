b96f9d130d44fc30646e10cc3e0d58b796a7ed76
Taqreem-k <md.taqreemkhan@gmail.com>
2026-05-04 23:43:55 +0530

    feat: add getme API, and Auth controller attatched to it

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 
 
 /**
@@ -56,7 +57,6 @@ async function registerUserController(req, res) {
 }
 
 
-
 /**
  * @name loginUserController
  * @description login a user, expects email and password in the request body
@@ -99,8 +99,50 @@ async function loginUserController(req,res) {
     })
 }
 
+/**
+ * @name logoutUserController
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+
+
+async function logoutUserController(req,res){
+    const token = req.cookies.token
+
+    if (token){
+        await tokenBlacklistModel.create({token})
+    }
+
+    res.clearCookie("token")
+
+    res.status(200).json({
+        message: "User logged out Succesfully"
+    })
+}
+
+/**
+ * @name getMeController
+ * @description get the current logged in user details.
+ * @access private
+ */
+
+async function getMeController(req,res){
+
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message: "user details fetched successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
 
 module.exports = {
     registerUserController,
-    loginUserController
+    loginUserController,
+    logoutUserController,
+    getMeController
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const authController = require("../controllers/auth.controller")
+const authMiddleware = require("../middlewares/auth.middleware")
 
 const authRouter = Router()
 
@@ -24,6 +25,14 @@ authRouter.post("/login",authController.loginUserController)
  * @description clear token from user cookie and add the token in blacklist
  * @access public
  */
-authRouter.get("/logout")
+authRouter.get("/logout, authController.logoutUserController")
+
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get("/get-me", authMiddleware.authUser,)
 
 module.exports = authRouter
