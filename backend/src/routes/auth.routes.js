9c6ecd5a9743b4ccc1c8ca8d93d32850540fe1cf
Taqreem-k <md.taqreemkhan@gmail.com>
2026-05-05 22:38:40 +0530

    feat: update fronend app and style; fix: update auth route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -33,6 +33,6 @@ authRouter.get("/logout, authController.logoutUserController")
  * @description get the current logged in user details
  * @access private
  */
-authRouter.get("/get-me", authMiddleware.authUser,)
+authRouter.get("/get-me", authMiddleware.authUser,authController.getMeController)
 
 module.exports = authRouter
